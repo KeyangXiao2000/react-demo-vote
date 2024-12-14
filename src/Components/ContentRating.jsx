0efae3a9d3197a9f118a3297c15fe2b9f84982cd
KeyangXiao2000 <keyang.xiao.2000@gmail.com>
2024-12-13 21:24:23 -0500

    Uploaded to GitHub

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,12 +5,34 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state = {
+      likes: 0,
+      dislikes: 0,
+      handleLike:() => {
+        this.setState((prev) => ({likes: prev.likes + 1}));
+      },
+      handleDislike:() => {
+        this.setState((prev) => ({dislikes: prev.dislikes + 1}));
+      }
+    };
   }
   render() {
     return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
+      <>
+      <div className='content-rating'>
+        <p>
+          (A Certain Topic)
+        </p>
+        <div className='rating-buttons'>
+          <button className="like-button" onClick={this.state.handleLike}>
+            Like ({this.state.likes})
+          </button>
+          <button className="dislike-button" onClick={this.state.handleDislike}>
+            Dislike ({this.state.dislikes})
+          </button>
+        </div>
+      </div>
+      </>
     );
   }
 }
